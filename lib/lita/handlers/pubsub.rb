8295d0a967176b4824bf2a5bde900e3522f612af
Tomas Varaneckas <tomas.varaneckas@gmail.com>
2017-10-31 21:19:56 +0200

    Implement more pubsub methods

--- lib/lita/handlers/pubsub.rb
+++ lib/lita/handlers/pubsub.rb
@@ -2,10 +2,17 @@ module Lita
   module Handlers
     class Pubsub < Handler
       route(
-        /^subscriptions/i,
+        /^subscriptions$/i,
         :subscriptions,
         command: true,
         help: { 'subscriptions' => 'shows current room subscriptions' }
+      )
+
+      route(
+        /^subscribers (of )?([a-z0-9\-\.\:_]+)$/i,
+        :subscribers,
+        command: true,
+        help: { 'subscriptions of EVENT' => 'shows rooms subscribed to EVENT' }
       )
 
       route(
@@ -22,18 +29,41 @@ module Lita
         help: { 'unsubscribe EVENT' => 'subscribes current channel to event' }
       )
 
+      route(
+        /^publish ([a-z0-9\-\.\:_]+) (.+)$/i,
+        :publish,
+        command: true,
+        help: { 'publish EVENT DATA' => 'publishes DATA to EVENT subscribers' }
+      )
+
+      on(:pubsub) do |payload|
+        event = payload[:event]
+        rooms = redis.smembers("pubsub.events.#{event}")
+        rooms.each do |room|
+          target = Source.new(room: room)
+          robot.send_message(target, payload[:data])
+        end
+      end
+
       def subscriptions(response)
         room = response.room
         return response.reply('This command only works in a room') unless room
-        subscriptions = redis.smembers("pubsub.subscriptions.#{room.id}")
+        subscriptions = redis.smembers("pubsub.rooms.#{room.id}")
         response.reply("Subscriptions for #{room.name}: #{subscriptions}")
+      end
+
+      def subscribers(response)
+        event = response.matches[0][1]
+        subscriptions = redis.smembers("pubsub.events.#{event}")
+        response.reply("Subscribers of #{event}: #{subscriptions}")
       end
 
       def subscribe(response)
         event = response.matches[0][0]
         room = response.room
         return response.reply('This command only works in a room') unless room
-        redis.sadd("pubsub.subscriptions.#{room.id}", event)
+        redis.sadd("pubsub.rooms.#{room.id}", event)
+        redis.sadd("pubsub.events.#{event}", room.id)
         response.reply("Subscribed #{room.name} to #{event} events")
       end
 
@@ -41,16 +71,22 @@ module Lita
         event = response.matches[0][0]
         room = response.room
         return response.reply('This command only works in a room') unless room
-        subscriptions = redis.smembers("pubsub.subscriptions.#{room.id}")
+        subscriptions = redis.smembers("pubsub.rooms.#{room.id}")
         if subscriptions.include?(event)
-          redis.srem("pubsub.subscriptions.#{room.id}", event)
-          response.reply("Unsubscribed #{room.name} to #{event} events")
+          redis.srem("pubsub.rooms.#{room.id}", event)
+          redis.srem("pubsub.events.#{event}", room.id)
+          response.reply("Unsubscribed #{room.name} from #{event} events")
         else
           response.reply(
             "There is no #{event} subscription in #{room.name}.\n" \
             "Current subscriptions: #{subscriptions}"
           )
         end
+      end
+
+      def publish(response)
+        event, data = response.matches[0]
+        robot.trigger(:pubsub, event: event, data: data)
       end
 
       Lita.register_handler(self)
--- spec/lita/handlers/pubsub_spec.rb
+++ spec/lita/handlers/pubsub_spec.rb
@@ -4,27 +4,43 @@ describe Lita::Handlers::Pubsub, lita_handler: true do
   let(:robot) { Lita::Robot.new(registry) }
   subject { described_class.new(robot) }
   let(:room) { Lita::Room.new('foos') }
+  let(:room2) { Lita::Room.new('bars') }
 
   it 'subscribes current channel to event' do
     send_message("lita subscribe foo", from: room)
-    expect(replies.last).to eq("Subscribed foos to foo events")
-
     send_message("lita subscriptions", from: room)
     expect(replies.last).to eq('Subscriptions for foos: ["foo"]')
   end
 
   it 'unsubscribes current channel from event' do
     send_message("lita subscribe foo", from: room)
-    expect(replies.last).to eq("Subscribed foos to foo events")
-
     send_message("lita subscriptions", from: room)
     expect(replies.last).to eq('Subscriptions for foos: ["foo"]')
 
     send_message("lita unsubscribe foo", from: room)
-    expect(replies.last).to eq("Unsubscribed foos to foo events")
+    expect(replies.last).to eq("Unsubscribed foos from foo events")
 
     send_message("lita subscriptions", from: room)
     expect(replies.last).to eq('Subscriptions for foos: []')
+  end
+
+  it 'shows subscribers of event' do
+    send_message("lita subscribe foo", from: room)
+    send_message("lita subscribe foo", from: room2)
+
+    send_message("lita subscribers foo")
+    expect(replies.last).to eq('Subscribers of foo: ["bars", "foos"]')
+
+    send_message("lita subscribers of foo")
+    expect(replies.last).to eq('Subscribers of foo: ["bars", "foos"]')
+  end
+
+  it 'publishes data to event subscribers' do
+    send_message("lita subscribe foo", from: room)
+    send_message("lita subscribe foo", from: room2)
+    send_message("lita publish foo bar de baz")
+    expect(replies[-1]).to eq('bar de baz')
+    expect(replies[-2]).to eq('bar de baz')
   end
 
   it 'does not fail when unsubscribing unsubscribed event' do
